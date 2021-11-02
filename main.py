82d094d85a15d845b57944cb30b37c641b1f6cf2
clyde0813 <drericcity@gmail.com>
2021-11-02 12:59:30 +0900

    좌석선택 완료

--- main.py
+++ main.py
@@ -51,8 +51,12 @@ class App(threading.Thread):
         self.round_label.grid(row=8, column=0)
         self.round_entry = Entry(self.object_frame, width=40)
         self.round_entry.grid(row=8, column=1)
+        self.seat_label = Label(self.object_frame, text="좌석 수")
+        self.seat_label.grid(row=9, column=0)
+        self.seat_entry = Entry(self.object_frame)
+        self.seat_entry.grid(row=9, column=1)
         self.test_button = Button(self.object_frame, text="테스트", width=3, height=2, command=self.seat_select)
-        self.test_button.grid(row=9, column=1)
+        self.test_button.grid(row=10, column=1)
         self.dp.mainloop()
 
     def login_go(self):
@@ -102,6 +106,7 @@ class App(threading.Thread):
 
     def seat_select(self):
         def task():
+            self.driver.switch_to.default_content()
             self.driver.switch_to.frame(self.driver.find_element_by_name("ifrmSeat"))
             self.driver.switch_to.frame(self.driver.find_element_by_name("ifrmSeatDetail"))
             self.wait.until(EC.presence_of_element_located(
@@ -109,7 +114,16 @@ class App(threading.Thread):
             seats = self.driver.find_elements_by_css_selector(
                 'img[src="http://ticketimage.interpark.com/TMGSNAS/TMGS/G/1_90.gif"]')
             print(len(seats))
-
+            if int(self.seat_entry.get()) > len(seats):
+                seat_count = len(seats)
+            else:
+                seat_count = int(self.seat_entry.get())
+            for i in range(0, seat_count):
+                seats[i].click()
+            print("좌석 선택 완료")
+            self.driver.switch_to.default_content()
+            self.driver.switch_to.frame(self.driver.find_element_by_name("ifrmSeat"))
+            self.driver.find_element_by_id("NextStepImage").click()
         newthread = threading.Thread(target=task)
         newthread.start()
 
